fb4c3bdc07a46e2702c298bb0794fd479bce2b01
Luis Liz <luis.liz@upr.edu>
2020-02-06 23:00:34 -0400

    Commit prepared for transfer testing

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 pg_config = {
     'user': 'postgres',
-    'password': '',
-    'dbname': '',
+    'password': 'LX2GkLPmjkU*xCMgRoGh',
+    'dbname': 'woctest',
     'host': 'wocpr.com',
     'port': '5432'
 }
@@ -11,7 +11,7 @@ mail_config =  {
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": 'email@gmail.com',
-    "MAIL_PASSWORD": ''
+    "MAIL_USERNAME": 'hacksquadpr@gmail.com',
+    "MAIL_PASSWORD": 'aF3$27Ug38'
 }
         
